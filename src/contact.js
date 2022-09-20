1b270059c779f83c398c2ab9524d6d3bb2e0dc16
newerk <juniusowens@yahoo.com>
2022-09-20 10:46:49 -0400

    Add function that Builds Contact Form into HTML

--- src/contact.js
+++ src/contact.js
@@ -1,3 +1,4 @@
+import './contact-page.css'
 //exports a div element that builds the contact page
 
 
@@ -21,6 +22,88 @@ export const contactInfo = () => {
     doorDash.textContent = 'doordash:filler text'
 
 
+    //left side of page
+    const leftSidePage = document.createElement('div');
+    const formContainer = document.createElement('form');
+    formContainer.className = 'form-container';
+
+    content.appendChild(formContainer);
+
+
+    //iterate through the array and build on the proper forms and the neccesarry content to make them work properly
+    const formLabels = ['First Name', 'Last Name', 'Phone Number', 'Email', 'Message'];
+
+    const formBuilder = ((arr) => {
+        const createLabel = (label, type) => {
+            const concat = label.toLowerCase().split(" ").join("");
+
+            const form = document.createElement('label');
+            const formInput = document.createElement('input');
+            formInput.type = type;
+            formInput.id = formInput.name = concat;
+
+            const labelName = document.createElement('p');
+            labelName.textContent = label;
+
+            form.appendChild(labelName);
+            form.appendChild(formInput);
+            formContainer.append(form);
+        }
+
+        //use a for loop and if/switch statements to create the DOM
+        for (const label of arr) {
+            switch (label) {
+                case 'First Name':
+                    createLabel(label, 'text');
+
+                    break;
+
+                case 'Last Name':
+                    createLabel(label, 'text');
+
+                    break;
+
+                case 'Phone Number':
+                    createLabel(label, 'tel');
+
+                    break;
+
+                case 'Email':
+                    createLabel(label, 'email');
+
+                    break;
+
+                case 'Message':
+
+                    const msgContainer = document.createElement('div');
+                    msgContainer.className = 'msg-container';
+
+                    const textarea = document.createElement('textarea');
+                    textarea.id = 'message';
+                    const labelName = document.createElement('p');
+                    labelName.textContent = 'Message';
+
+                    msgContainer.appendChild(labelName);
+                    msgContainer.appendChild(textarea);
+                    formContainer.appendChild(msgContainer);
+
+                    break;
+
+            }
+        }
+
+        const submitBtn = document.createElement('button');
+        submitBtn.textContent = 'Submit';
+        formContainer.appendChild(submitBtn);
+
+
+
+        leftSidePage.appendChild(formContainer);
+        leftSidePage.className = "left-of-page"
+        content.appendChild(leftSidePage);
+    })(formLabels);
+
+
     apps.appendChild(uberEats);
     apps.appendChild(grubHub);
     apps.appendChild(doorDash);
@@ -30,3 +113,6 @@ export const contactInfo = () => {
     content.appendChild(phoneNum);
     content.appendChild(apps);
 };
+
+
+
