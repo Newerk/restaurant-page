8fc5cd542ef9bbe54663d32bcee0a46887777699
newerk <juniusowens@yahoo.com>
2022-09-21 11:05:17 -0400

    Add Image of Map to Contact Page Right Side

--- src/contact/right-half.js
+++ src/contact/right-half.js
@@ -3,5 +3,11 @@ export const contactRight = () => {
 
     //left side of page
     const rightSidePage = document.createElement('div');
+    rightSidePage.className = 'right-of-page';
+    const map = document.createElement('img');
+    map.id = 'map';
+
+    rightSidePage.appendChild(map);
+    content.appendChild(rightSidePage);
 
 }
